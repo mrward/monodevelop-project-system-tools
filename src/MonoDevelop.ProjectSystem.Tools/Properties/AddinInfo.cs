f04ab451fb34fbc644e98ac5cebc208ed4f5a12c
Matt Ward <matt.ward@microsoft.com>
2023-04-01 11:05:17 +0100

    Update addin version to 0.13

--- src/MonoDevelop.ProjectSystem.Tools/Properties/AddinInfo.cs
+++ src/MonoDevelop.ProjectSystem.Tools/Properties/AddinInfo.cs
@@ -28,7 +28,7 @@ using Mono.Addins;
 [assembly: Addin (
 	"ProjectSystemTools",
 	Namespace = "MonoDevelop",
-	Version = "0.12",
+	Version = "0.13",
 	Category = "IDE extensions")]
 
 [assembly: AddinName ("Project System Tools")]
